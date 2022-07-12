41be6df521329e2f9f94ac30ce07557dd0ef4a87
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-07-12 15:47:44 +1000

    adding a switch statement

--- src/components/content.js
+++ src/components/content.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Navigation from './navigation';
 import Header from './pages/header';
 import Projects from './pages/projects';
 import AboutMe from './pages/about-me';
@@ -6,13 +7,24 @@ import AboutMe from './pages/about-me';
 
 
 function Content() {
-    return (
-        <div className="Content">
-            <Header />
-            <Projects />
-            <AboutMe />
-        </div>
-    );
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'AboutMe': return <AboutMe />;
+      case 'Projects': return <Projects />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div className="Content">
+      <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+      <Header />
+      {renderPage()}
+    </div>
+  );
 }
 
 export default Content;
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,13 +1,21 @@
 import React from "react";
 
-function Navigation() {
+function Navigation({ currentPage, handlePageChange }) {
   return (
     <div className="top-bar">
       <ul className="nav-list">
-        <li className="nav-list-item">About Me</li>
-        <li className="nav-list-item">Portfolio</li>
-        <li className="nav-list-item">Contact</li>
-        <li className="nav-list-item">Resume</li>
+        <li className="nav-list-item"><a href="#about-me" onClick={() => handlePageChange('AboutMe')} className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}>
+          About Me
+        </a></li>
+        <li className="nav-list-item"><a href="#projects" onClick={() => handlePageChange('Projects')} className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
+          Portfolio
+        </a></li>
+        <li className="nav-list-item"><a href="#contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+          Contact
+        </a></li>
+        <li className="nav-list-item"><a href="#resume" onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>
+          Resume
+        </a></li>
       </ul>
     </div>
   );
