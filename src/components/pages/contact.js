9250a54c6aa65cd4a18f7d757e5c3f09b145213f
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-07-13 16:53:36 +1000

    testing contact page

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { isValidEmail } from '../../helper';
 
+// refer to form part of student mini project week 20 //
 function Contact() {
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [error, setError] = useState('');
+
+  // pasing in state change arguements and running if statements agaisnt each input field to set the answer as the value inside the field //
+  const handleChange = (e) => {
+    const { target } = e;
+    const type = target.name;
+    const value = target.value;
+    if (type === 'name') {
+      setUser(value);
+    } else if (type === 'email') {
+      setEmail(value);
+    } else {
+      setMessage(value);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // handled submit, now to make sure all fields have something typed in them //
+    if (!user) {
+      setError('Please enter your name');
+      return;
+    }
+    if (!isValidEmail(email)) {
+      setError('Please enter a valid email address');
+      return;
+    }
+    if (!message) {
+      setError('Please enter a message');
+      return;
+    }
+
+    setUser('');
+    setEmail('');
+    setMessage('');
+  }
+
+
+
+
   return (
     <div className="Contact">
-      <form>
-
+      <form id="form">
+        <div className="form-group">
+          <label for="userInput">Name</label>
+          <input value={user} onChange={handleChange} type="user" className="form-control" id="userInput" placeholder="Please enter your name" />
+        </div>
+        <div className="form-group">
+          <label for="emailInput">Email address</label>
+          <input value={email} onChange={handleChange} type="email" className="form-control" id="emailInput" placeholder="Please enter an email address" />
+        </div>
+        <div className="form-group">
+          <label for="messageInput">Message</label>
+          <textarea value={message} onChange={handleChange} className="form-control" id="messageInput" rows="3"></textarea>
+        </div>
+        <button onClick={handleSubmit} type="submit" className="btn btn-primary">Submit</button>
       </form>
     </div>
   );
